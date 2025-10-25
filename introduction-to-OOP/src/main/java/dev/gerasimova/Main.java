b27b76799d1136057d25c37b2266e037153c67b5
GerasimovaAnastasia <asia-barbarisca30@yandex.ru>
2025-10-25 03:28:57 +0300

    feat: added task 7 - introduction to OOP

---
+++ introduction-to-OOP/src/main/java/dev/gerasimova/Main.java
@@ -0,0 +1,12 @@
+package dev.gerasimova;
+
+/**
+ * Введение в ООП.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        ServiceStudent.demonstrateStudentWorkflow();
+
+    }
+}
