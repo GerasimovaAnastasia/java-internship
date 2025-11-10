9729a482efe8b0ed10efa4278db10c5d14da7593
GerasimovaAnastasia <asia-barbarisca30@yandex.ru>
2025-11-10 18:59:12 +0300

    feat: task 1 - создание проекта через Spring Initializr

---
+++ introduction-spring-framework/src/main/java/dev/gerasimova/introduction_spring_framework/IntroductionSpringFrameworkApplication.java
@@ -0,0 +1,14 @@
+package dev.gerasimova.introduction_spring_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Точка входа веб-приложения.
+ */
+@SpringBootApplication
+public class IntroductionSpringFrameworkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionSpringFrameworkApplication.class, args);
+	}
+}
