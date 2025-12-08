2f55267018873578c5066623f1e44b3eb20c8acf
GerasimovaAnastasia <asia-barbarisca30@yandex.ru>
2025-12-08 13:12:13 +0300

    feat: add Swagger documentation for notification-service

--- notification-service/src/main/java/dev/gerasimova/dto/NotificationRequest.java
+++ notification-service/src/main/java/dev/gerasimova/dto/NotificationRequest.java
@@ -1,5 +1,6 @@
 package dev.gerasimova.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -8,9 +9,22 @@ import jakarta.validation.constraints.Size;
  * @param username - логин пользователя
  * @param message - сообщение пользователя
  */
+@Schema(description = "Запрос на создание уведомления")
 public record NotificationRequest(
+        @Schema(
+                description = "Логин пользователя",
+                example = "admin1",
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
         @NotBlank(message = "username не может быть пустым")
         String username,
+        @Schema(
+                description = "Текст уведомления",
+                example = "Добавлена новая книга",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                minLength = 3,
+                maxLength = 50
+        )
         @NotBlank(message = "message не может быть пустым")
         @Size(min = 3, max = 50, message = "Сообщение должно быть от 3-50 символов")
         String message
--- notification-service/src/main/java/dev/gerasimova/dto/NotificationResponse.java
+++ notification-service/src/main/java/dev/gerasimova/dto/NotificationResponse.java
@@ -1,4 +1,7 @@
 package dev.gerasimova.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * Дто для ответа пользователю на запрос о создании уведомления.
  *
@@ -6,8 +9,22 @@ package dev.gerasimova.dto;
  * @param message - сообщение пользователя
  * @param notificationId - Id сообщения
  */
+@Schema(description = "Ответ на запрос создания уведомления")
 public record NotificationResponse(
+        @Schema(
+                description = "Статус обработки уведомления",
+                example = "SUCCESS",
+                allowableValues = {"SUCCESS", "ERROR", "PENDING"}
+        )
         String status,
+        @Schema(
+                description = "Сообщение для пользователя",
+                example = "Уведомление поставлено в очередь"
+        )
         String message,
+        @Schema(
+                description = "Уникальный идентификатор уведомления",
+                example = "1"
+        )
         String notificationId) {
 }
