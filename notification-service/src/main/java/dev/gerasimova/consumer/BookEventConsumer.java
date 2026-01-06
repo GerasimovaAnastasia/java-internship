c48c7fcbdb387edbef07548638de3451ba1e8b90
GerasimovaAnastasia <asia-barbarisca30@yandex.ru>
2026-01-07 01:51:41 +0300

    feat: add task-7 add asynchrony inside Consumer

--- notification-service/src/main/java/dev/gerasimova/consumer/BookEventConsumer.java
+++ notification-service/src/main/java/dev/gerasimova/consumer/BookEventConsumer.java
@@ -1,36 +1,31 @@
 package dev.gerasimova.consumer;
 
 import dev.gerasimova.dto.BookCreatedEvent;
-import dev.gerasimova.model.Notification;
-import dev.gerasimova.repository.NotificationRepository;
+import dev.gerasimova.service.NotificationConsumerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class BookEventConsumer {
-    private final NotificationRepository notificationRepository;
-
+    private final NotificationConsumerService notificationConsumerService;
     @KafkaListener(topics = "book_events")
     public void handleBookCreatedEvent(BookCreatedEvent event) {
         log.info("Получен BookCreatedEvent: {}", event);
-        try {
-            Notification notification = new Notification(event);
-
-            Notification savedNotification = notificationRepository.save(notification);
-
-            log.info("Уведомление сохранено в MongoDB. ID: {}, Book ID: {}",
-                    savedNotification.getId(), savedNotification.getBookId());
-
-            log.info("   Book ID: {}", event.getBookId());
-            log.info("   Title: {}", event.getTitle());
-            log.info("   Author: {}", event.getAuthorSurname());
-            log.info("   CreatedAt: {}", event.getCreatedAt());
-
-        } catch (Exception e) {
-            log.error("Ошибка при сохранении уведомления в MongoDB: {}", e.getMessage(), e);
-        }
+        CompletableFuture<Void> future = notificationConsumerService
+                .processBookCreatedEvent(event)
+                .thenAccept(savedNotification -> {
+                    log.info("Асинхронная задача успешно завершена для Book ID: {}",
+                            savedNotification.getBookId());
+                })
+                .exceptionally(throwable -> {
+                    log.error("Ошибка асинхронной задачи: {}", throwable.getMessage());
+                    return null;
+                });
     }
 }
