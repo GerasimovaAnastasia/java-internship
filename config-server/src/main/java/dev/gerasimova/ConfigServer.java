666e6c413874c9687f10ba88aa514ea49eab1efc
GerasimovaAnastasia <asia-barbarisca30@yandex.ru>
2026-01-13 18:45:31 +0300

    feat: add task-2-3 add a сonfig-server to read the config-repository

---
+++ config-server/src/main/java/dev/gerasimova/ConfigServer.java
@@ -0,0 +1,14 @@
+package dev.gerasimova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
