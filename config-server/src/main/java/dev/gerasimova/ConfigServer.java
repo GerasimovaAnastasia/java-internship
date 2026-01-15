e11bd9403245c1a6d28689bff571e4b495c70dae
GerasimovaAnastasia <asia-barbarisca30@yandex.ru>
2026-01-15 20:10:49 +0300

    feat: add task-9 add RefreshScope

--- config-server/src/main/java/dev/gerasimova/ConfigServer.java
+++ config-server/src/main/java/dev/gerasimova/ConfigServer.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
